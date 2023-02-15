8cee1728965bb93f2501b30ce99a5ca765587bd4
MaxiStahl1992 <76917082+MaxiStahl1992@users.noreply.github.com>
2023-02-15 18:08:15 +0100

    change server name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -61,7 +61,7 @@ function App() {
         if (profileObj) {
           
             const response = await fetch(
-                "http://localhost:8080/api/v1/users",
+                "https://refine-server.onrender.com",
                 {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
@@ -131,7 +131,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://refine-server.onrender.com")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
